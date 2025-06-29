18cc651f8c828fad5ba3e393ac48002c1a6d1d0f
unknown <gustavoapolinio123@gmail.com>
2025-06-29 19:56:31 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string resposta = " Eu sou Groot";
+string pergunta = "";
+Console.Clear();
+Console.WriteLine(@"--- Groot Chatbot ---");
+
+while (pergunta != "tchau")
+{
+    Console.Write("Pergunta:   "); pergunta = Console.ReadLine()!.ToLower();
+    if (pergunta != "tchau")
+    {
+    Console.WriteLine($"     Resposta: {resposta}.");}
+
+    else if (pergunta == "tchau")
+    {
+        Console.WriteLine($"     Resposta:{resposta}!");
+    }
+}
